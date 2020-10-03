4e0236aec0dfadbf13189b872e4c9294313ac62c
Shubham <skpawar1305@gmail.com>
2020-10-03 07:29:41 +0000

    CustomDoze: add an extra condition for SmartRaiseToWake
    fixes screen waking up when Smart Wake is on even if LiftToCheck is off
    
    Change-Id: I4267cbdae1298759de46e266a06c4b3afad6c4c2

--- src/com/custom/ambient/display/TiltSensor.java
+++ src/com/custom/ambient/display/TiltSensor.java
@@ -84,6 +84,7 @@ public class TiltSensor implements SensorEventListener {
 
     @Override
     public void onSensorChanged(SensorEvent event) {
+        boolean isLiftToCheck = Utils.tiltGestureEnabled(mContext);
         boolean isRaiseToWake = Utils.isRaiseToWakeEnabled(mContext);
         boolean isSmartScreenWake = Utils.isSmartScreenWakeEnabled(mContext);
 
@@ -109,7 +110,7 @@ public class TiltSensor implements SensorEventListener {
                     Utils.launchDozePulse(mContext);
                 }
             }
-            if (isSmartScreenWake) {
+            if (isLiftToCheck && isSmartScreenWake) {
                 mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_TILT_DETECTOR);
                 if (event.values[0] == 0) {
                     mSensor = Utils.getSensor(mSensorManager, "qti.sensor.amd");
